31187e1b034ce23efb0213bc67c1e6d678cf2c3c
TngPrag <130535778+TngPrag@users.noreply.github.com>
2024-07-01 14:16:46 +0300

    Update teltonika.go

--- teltonika.go
+++ teltonika.go
@@ -1,8 +1,4 @@
-// Copyright 2022 Alim Zanibekov
-//
-// Use of this source code is governed by an MIT-style
-// license that can be found in the LICENSE file or at
-// https://opensource.org/licenses/MIT.
+
 
 package teltonika
 
